954427d31d2d0c478fd3bca4fcfb5a2eae0d2e50
ShafiqTanim <shafiqullah715@gmail.com>
2024-10-01 14:24:54 +0600

    auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
+
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
 import Booking from './Pages/Booking';
 import Dashboard from './Pages/Dashboard';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Protected from './components/protected';
+
 function App() {
+  const isSignedIn = localStorage.getItem("access_token") || false;
   return (
     <BrowserRouter>
         <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/booking" element={<Booking />} />
         </Routes>
